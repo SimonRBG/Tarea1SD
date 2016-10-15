1895d37f4aea5563a647d826f0e9edc49bd037f2
Simon Rabourg <simon.rabourg@ensimag.grenoble-inp.fr>
2016-10-15 19:37:08 -0300

    Ipoint.java : creation of the class to solve problem with utilisation of Unremote objects in MainThreads. We have to use RemoteObjects in Points functions's argument. Implemented modifications in Point.java.

--- src/cl/uchile/dcc/cc5303/Point.java
+++ src/cl/uchile/dcc/cc5303/Point.java
@@ -1,21 +1,36 @@
 package cl.uchile.dcc.cc5303;
 
-public class Point {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Point extends UnicastRemoteObject implements IPoint {
     public int x,y;
     public boolean visible;
 
     public static final int dHip = 4;
 
-    public Point(int x, int y) {
+    public Point(int x, int y) throws RemoteException{
         this.x = x;
         this.y = y;
         this.visible = true;
     }
 
-    public Point(int x, int y, boolean visibility) {
+    public Point(int x, int y, boolean visibility) throws RemoteException{
         this.x = x;
         this.y = y;
         this.visible = visibility;
+    }
+
+    public int getX() throws RemoteException {
+        return x;
+    }
+
+    public int getY() throws RemoteException {
+        return y;
+    }
+
+    public boolean getVisible() throws RemoteException {
+        return visible;
     }
 
 }
